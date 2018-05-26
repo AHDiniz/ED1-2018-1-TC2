cb99bc42a8fe2a81736589b3bb630d1d1e383af1
AHDiniz <AHDiniz@github.com>
2018-05-26 15:18:42 -0300

    Adicionando Makefile p/ Windows

---
+++ Windows.mk
@@ -0,0 +1,20 @@
+# Makefile para Windows
+
+CC = gcc
+
+SRC = $(wildcard *.c)
+
+OBJ = $(SRC:.c=.0)
+
+EXE = compact.exe
+
+CFLAGS = -Wall -g -O3 -lmingw32
+
+$(EXE): $(OBJ)
+	$(CC) -o $@ $^ $(CFLAGS)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $^
+
+clean:
+	rm *.o

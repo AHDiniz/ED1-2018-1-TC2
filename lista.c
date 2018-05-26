a0f62247d23af1e4c5cca32abf97a519fc364794
AHDiniz <AHDiniz@github.com>
2018-05-26 11:28:41 -0300

    Adicionando lista genérica

---
+++ lista.c
@@ -0,0 +1,217 @@
+/********************************************
+Trabalho ED1 2018/1: Sistema colaborativo
+
+Alan Herculano Diniz e Rafael Belmock Pedruzzi
+
+lista.c: implementações para lista genérica
+********************************************/
+
+#include "lista.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Implementando uma lista genérica:
+struct lista
+{
+    Item* primeiro;           // Ponteiro para o primeiro item da lista
+    Item* ultimo;             // Ponteiro para o último item da lista
+    char* tipo;               // Tag com o tipo de item que a lista contém
+    unsigned int comprimento; // Comprimento da lista (quantidade de itens)
+};
+
+// Implementando um item de uma lista genérica:
+struct item
+{
+    void *conteudo; // Ponteiro para o conteúdo do item
+    char *tipo;     // Tag com o tipo do conteúdo do item
+    Item *proximo;  // Ponteiro para o próximo item da lista
+    int posicao;    // Posição do item na lista
+};
+
+// Funções estáticas:
+
+// Função de comparação de tipos entre lista e item:
+static int CmpTipoListaItem(Lista* lista, Item* item)
+{
+    if (strcmp(lista->tipo, item->tipo) == 0)
+        return 1;
+    else
+        return 0;
+}
+
+// Alocando uma nova lista dinamicamente:
+Lista* NovaLista(const char* tipo)
+{
+    Lista* lista = (Lista*)malloc(sizeof(Lista)); // Alocando dinamicamente o espaço de memória necessário para a nova lista
+    
+    lista->tipo = (char*)malloc(strlen(tipo) + 1); // Alocando dinamicamente o espaço necessário para a tag de tipo da lista
+    strcpy(lista->tipo, tipo); // Copiando o conteúdo da string de entrada para o campo do struct
+
+    lista->primeiro = lista->ultimo = NULL; // A lista está vazia (não há nada nem na primeira nem na última posição)
+
+    lista->comprimento = 0; // A lista tem tamanho zero
+
+    return lista;
+}
+
+// Destruindo a lista na memória:
+void DestroiLista(Lista* lista, FreeContItem Func)
+{
+    int i, tamanho;
+    // Verificando se a lista não está vazia para destruir cada item ainda existente:
+    if (!ListaVazia(lista))
+    {
+        for (i = 0, tamanho = lista->comprimento; i < tamanho; i++) // Varrendo a lista para destruir os itens
+            ListaRemove(lista, 0, Func); // Removendo cada item da lista até ela ficar vazia
+    }
+    free(lista->tipo); // Liberando a tag de tipo da lista
+    free(lista); // Liberando a lista em si
+}
+
+// Verificação de lista vazia:
+int ListaVazia(Lista* lista)
+{
+    return (lista->comprimento == 0); // Se o comprimento da lista é igual a zero, então não há nenhum item nela
+}
+
+// Tamanho da lista:
+int TamanhoLista(Lista* lista)
+{
+    return (lista->comprimento);
+}
+
+// Adicionando um item na lista:
+void ListaAdd(Lista* lista, Item* item)
+{
+    if (!CmpTipoListaItem(lista, item)) // Comparando os tipos da lista e do item
+    {
+        printf("Tentando inserir um item de um tipo diferente da lista.\n");
+        return; // Se forem diferentes, o item não será adicionado na lista
+    }
+    if (ListaVazia(lista)) // Se a lista for vazia
+        lista->primeiro = item; // O primeiro item da lista vai ser o item de entrada da função
+    else // Se a lista possuir algum elemento
+        lista->ultimo->proximo = item; // O próximo item do atual último da lista é o item de entrada
+    lista->ultimo = item; // O último item da lista será o item de entrada
+    item->posicao = lista->comprimento; // Definindo a posicao do item de entrada na lista
+    lista->comprimento++; // Atualizando o tamanho da lista
+}
+
+// Removendo um item da lista:
+void ListaRemove(Lista* lista, const unsigned int pos, FreeContItem Func)
+{
+    int i;
+    if (pos == 0) // Se o item a ser retirado é o primeiro
+    {
+        Item* alvo = lista->primeiro; // Auxiliar para não perder a referência do item a ser removido
+        lista->primeiro = lista->primeiro->proximo; // Atualizando o valor do primeiro item da lista
+        Item* item = lista->primeiro;
+        // Atualizando o valor das posições dos itens da lista:
+        for (i = 0; i < lista->comprimento - 1; i++)
+        {
+            item->posicao--;
+            item = item->proximo;
+        }
+        LiberaItem(alvo, Func); // Liberando o item alvo da memória
+    }
+    else
+    {
+        if (pos == lista->comprimento - 1) // Se o item a ser retirado é o último
+        {
+            Item* anterior = lista->primeiro; // Auxiliar que apontará para o novo último item
+            Item* alvo = lista->ultimo; // Auxiliar para não perder a referência do item a ser removido
+            for (i = 0; i < lista->comprimento - 1; i++) // Percorrendo a lista para achar o penúltimo item
+                anterior = anterior->proximo;
+            // Atualizando o valor do último item da lista:
+            lista->ultimo = anterior;
+            anterior->proximo = NULL;
+            // Liberando o item alvo da memória:
+            LiberaItem(alvo, Func);
+        }
+        else
+        {
+            // Variáveis auxiliares que ajudarão a percorrer a lista:
+            Item* anterior = lista->primeiro;
+            Item* atual = lista->primeiro->proximo;
+            for (i = 1; i < lista->comprimento -1; i++) // Percorrendo cada item da lista
+                if (atual->posicao == pos) // Se o item atual estiver na posição desejada
+                    break; // O loop será quebrado
+                else
+                {
+                    // Atualizando os valores das variáveis auxiliares:
+                    anterior = atual;
+                    atual = atual->proximo;
+                }
+            anterior->proximo = atual->proximo; // Atualizando os valores dos ponteiros dos itens que continuam na lista
+            // Atualizando os valores das posições dos itens da lista:
+            anterior = anterior->proximo; // Primeiro item que precisa ter sua posição atualizada
+            for (i += 1; i < lista->comprimento - 1; i++)
+            {
+                anterior->posicao--;
+                anterior = anterior->proximo;
+            }
+            LiberaItem(atual, Func); // Liberando o item procurando da memória (vai acabar sendo retirado da lista)
+        }
+    }
+    lista->comprimento--; // Atualizando o valor do comprimento da lista
+}
+
+// Achando um item na lista:
+void* AchaItem(Lista* lista, const unsigned int pos)
+{
+    Item* ret = lista->primeiro; // Ponteiro que apontará para o item procurado
+    // Procurando o item desejado:
+    for (int i = 0; i < lista->comprimento; i++)
+        if (ret->posicao == pos)
+            break;
+        else
+            ret = ret->proximo;
+    return ret->conteudo; // Retornando o conteudo do item procurado
+}
+
+// Alocando um novo item dinamicamente:
+Item* NovoItem(const char* tipo, void* conteudo)
+{
+    Item *item = (Item *)malloc(sizeof(Item)); // Alocando o espaço necessário para um novo item dinamicamente
+
+    item->conteudo = conteudo; // Fazendo o conteudo do item apontar para o conteudo desejado
+
+    item->tipo = (char *)malloc(strlen(tipo) + 1); // Alocando o espaço necessário para a tag do tipo do item dinamicamente
+    strcpy(item->tipo, tipo);                      // Definindo a tag do tipo do item
+
+    item->proximo = NULL; // o proximo item só será deifnido quando o item estiver numa lista
+
+    item->posicao = 0; // Inicializando a posição do item com um placeholder
+
+    return item;
+}
+
+// Verificação de item com conteúdo opaco:
+int ItemVazio(Item* item)
+{
+    return (item->conteudo == NULL); // Verificando se o conteudo apontado pelo respectivo ponteiro do item é opaco
+}
+
+// Liberação de item da memória:
+int LiberaItem(Item* item, FreeContItem Func)
+{
+    if (ItemVazio(item)) // Checando se o item possui conteúdo
+    {
+        printf("O item nao possui conteudo.\n");
+        return 0;
+    }
+    if(Func != NULL) // Checando se o ponteiro para a função existe
+    {
+        Func(item->conteudo); // Liberando o conteúdo do item
+    }
+    free(item->tipo); // Liberando a tag de tipo do item
+    free(item); // Liberando o item
+    return 1;
+}
+
+// Conteúdo de um item:
+void* ConteudoItem(Item* item)
+{
+    return item->conteudo;
+}
---
+++ lista.h
@@ -0,0 +1,124 @@
+/********************************************
+Trabalho ED1 2018/1: Sistema colaborativo
+
+Alan Herculano Diniz e Rafael Belmock Pedruzzi
+
+lista.h: definições para lista genérica
+********************************************/
+
+#ifndef LISTA_H_
+
+#define LISTA_H_
+
+// Definindo um novo tipo de lista:
+typedef struct lista Lista;
+
+// Definindo um tipo para o item da lista:
+typedef struct item Item;
+
+// Definindo o padrão de função que libera o conteúdo de um item:
+typedef void FreeContItem(void*);
+
+// Definindo padrão de função para impressão de conteúdo de item:
+typedef void Imprime(void*);
+
+/*
+Nova Lista:
+- Entrada: a string que indica o tipo de item que a lista comporta;
+- Saída: um ponteiro para uma nova lista vazia;
+- Pré-condições: o tipo deve ser algum existente no programa e preferencialmente escrito da mesma forma em que o tipo foi definido;
+- Pós-condições: a lista está vazia;
+*/
+Lista* NovaLista(const char* tipo);
+
+/*
+Destruição de Lista:
+- Entrada: um ponteiro para a lista que será destruída e outro para a função de liberação de conteúdo de item;
+- Saída: nenhuma;
+- Pré-condições: a lista deve estar alocada na memória;
+- Pós-condições: os itens da lista que ainda estão nela serão destruídos também;
+*/
+void DestroiLista(Lista* lista, FreeContItem Func);
+
+/*
+Verificação de Lista Vazia:
+- Entrada: a lista a ser verificada;
+- Saída: um inteiro de comportamento booleano que diz se a lista está vazia ou não;
+- Pré-condições: a lista precisa estar alocada na memória;
+- Pós-condições: nenhum efeito colateral;
+*/
+int ListaVazia(Lista* lista);
+
+/*
+Tamanho da lista:
+- Entrada: ponteiro para a lista a ser analisada;
+- Sáida: tamanho da lista analisada;
+- Pré-condições: lista alocada e não vazia;
+- Pós-condições: nenhum efeito colateral;
+*/
+int TamanhoLista(Lista* lista);
+
+/*
+Adicionar item na lista:
+- Entrada: a lista e o item a ser adicionado;
+- Saída: nenhuma;
+- Pré-condições: a lista e o item não podem estar vazios;
+- Pós-condições: o item é adicionado na lista e ela fica maior;
+*/
+void ListaAdd(Lista* lista, Item* item);
+
+/*
+Remover item na lista:
+- Entrada: a lista e a posição do item a ser removido;
+- Saída: nenhuma;
+- Pré-condições: a lista não pode estar vazia e a posição deve ficar entre zero e o tamanho atual da lista;
+- Pós-condições: o item é liberado da memória e a lista fica menor;
+*/
+void ListaRemove(Lista* lista, const unsigned int pos, FreeContItem Func);
+
+/*
+Achar item na lista:
+- Entrada: ponteiro para a lista e posição do item desejado;
+- Saída: ponteiro para o item procurado;
+- Pré-condições: lista alocada e não vazia e posição válida;
+- Pós-condições: nenhum efeito colateral;
+*/
+void* AchaItem(Lista* lista, const unsigned int pos);
+
+/*
+Criação de um novo item:
+- Entrada: tag com o tipo do item e o ponteiro opaco para o conteúdo;
+- Saída: um ponteiro para um novo item;
+- Pré-condições: tanto a tag quanto o conteúdo não podem apontar para NULL;
+- Pós-condições: nenhum efeito colateral;
+*/
+Item* NovoItem(const char* tipo, void* conteudo);
+
+/*
+Verificação de item com conteúdo opaco:
+- Entrada: o item a ser verificado;
+- Saída: um inteiro com comportamento booleano que diz se o item tem ou não um conteúdo opaco;
+- Pré-condições: o item já deve ter sido alocado;
+- Pós-condições: nenhum efeito colateral;
+*/
+int ItemVazio(Item* item);
+
+/*
+Liberação de item da memória:
+- Entrada: o item a ser liberado da memória e o ponteiro para a função que libera o conteúdo;
+- Sáida: um inteiro com comportamento booleano que diz se operação ocorreu ou não da forma devida;
+- Pré-condições: o item deve estar alocado na memória e a função deve existir e seguir o padrão determinado para a função que libera o conteúdo de um item;
+- Pós-condições: o item está vazio;
+*/
+int LiberaItem(Item* item, FreeContItem Func);
+
+/*
+Conteúdo de um item:
+- Entrada: ponteiro para o item;
+- Saída: ponteiro opaco para conteúdo:
+- Pré-condições: o item deve estar alocado e com conteúdo;
+- Pós-condições: nenhum efeito colateral;
+*/
+void* ConteudoItem(Item* item);
+
+#endif

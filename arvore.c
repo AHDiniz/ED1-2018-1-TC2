d586abe01f223d45d0c946d16340f242f057fff3
AHDiniz <AHDiniz@github.com>
2018-05-26 14:54:02 -0300

    Adicionando Árvore e Bitmap

---
+++ arvore.c
@@ -0,0 +1,47 @@
+#include "arvore.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+struct arvore
+{
+    Arvore* esq;
+    Arvore* dir;
+    char caracter;
+    int ocorrencias;
+};
+
+// Criando uma folha (árvore sem nós filhos):
+Arvore* CriaFolha(char caracter, int ocorrencias)
+{
+    Arvore* a = (Arvore*)malloc(sizeof(Arvore)); // Alocando espaço na memória dinamicamente
+    // Populando os campos da estrutura:
+    a->caracter = caracter;
+    a->ocorrencias = ocorrencias;
+    a->dir = a->esq = NULL;
+    return a; // Retornando o nó
+}
+
+// Criando uma árvore com nós filhos:
+Arvore* CriaArvore(char caracter, int ocorrencias, Arvore* esq, Arvore* dir)
+{
+    Arvore* a = (Arvore*)malloc(sizeof(Arvore)); // Alocando espaço na memória dinamicamente
+    // Populando os campos da estrutura:
+    a->caracter = caracter;
+    a->ocorrencias = ocorrencias;
+    a->esq = esq;
+    a->dir = dir;
+    return a; // Retornando o nó
+}
+
+// Verificando se uma árvore é um nó folha:
+int EhFolha(Arvore* arvore)
+{
+    return (arvore->esq == NULL && arvore->dir == NULL);
+}
+
+// Apagando a árvore:
+Arvore* DestroiArvore(Arvore* raiz)
+{
+    
+}
---
+++ arvore.h
@@ -0,0 +1,52 @@
+#ifndef ARVORE_H_
+
+#define ARVORE_H_
+
+/**
+ * Tipo Árvore:
+ * Será usada para a codificação dos caracteres em um texto;
+ * Possui os seguintes campos:
+ * Arvore* esq: ponteiro para a subárvore da esquerda;
+ * Arvore* dir: ponteiro para a subárvore da direita;
+ * char caracter: caracter analisado;
+ * int ocorrencias: número de ocorrências do caracter;
+*/
+typedef struct arvore Arvore;
+
+/**
+ * Função de criação de árvore sem nós filhos:
+ * Input: caracter e número de ocorrências do caracter;
+ * Output: ponteiro para uma folha (árvore sem nós filhos);
+ * Condições: nenhuma;
+ * Efeitos Colaterais: nenhum;
+*/
+Arvore* CriaFolha(char caracter, int ocorrencias);
+
+/**
+ * Função que cria uma árvore:
+ * Inputs: caracter, número de ocorrências do caracter, ponteiro para a subárvore da esquerda e para a da direita;
+ * Output: ponteiro para uma árvore com nós filhos;
+ * Condições: subárvores existentes e alocadas;
+ * Efeitos Colaterais: nenhum;
+*/
+Arvore* CriaArvore(char caracter, int ocorrencias, Arvore* esq, Arvore* dir);
+
+/**
+ * Função que verifica se a árvore é uma folha ou não:
+ * Input: ponteiro para a árvore;
+ * Output: inteiro com comportamento booleano que diz se a árvore é ou não uma folha;
+ * Condições: árvore existente e alocada;
+ * Efeitos Colaterais: nenhum;
+*/
+int EhFolha(Arvore* arvore);
+
+/**
+ * Função que destrói uma árvore da memória:
+ * Input: ponteiro para o nó raiz da árvore;
+ * Output: ponteiro vazio, caso a árvore seja apagada, ou o ponteiro da árvore caso aconteça algum erro;
+ * Condições: árvore existente e alocada;
+ * Efeitos Colaterais: a árvore será apagada da memória;
+*/
+Arvore* DestroiArvore(Arvore* raiz);
+
+#endif

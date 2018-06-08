92bdd8f1d356696c264744b0d070fcea12245fd3
RafaelPedruzzi <rafaelbelmockpedruzzi@hotmail.com>
2018-06-08 16:31:53 +0100

    listacaminho comentado

--- listacaminho.c
+++ listacaminho.c
@@ -25,12 +25,12 @@ struct caminho
 */
 static void DestroiCaminho(void* caminho)
 {
-    Caminho* cam = (Caminho*) caminho;
+    Caminho* cam = (Caminho*) caminho; // convertendo para Caminho
 
-    if(cam != NULL)
+    if(cam != NULL) // medida de segurança
     {
-        Lista_DestroiLista(cam->caminho,NULL);
-        free(cam);
+        Lista_DestroiLista(cam->caminho,NULL); // destroi a lista do caminho
+        free(cam); // libera o struct
     }
 }
 
@@ -43,26 +43,28 @@ static void DestroiCaminho(void* caminho)
 */
 static void CriaCaminhos(Lista* lista, Arvore* huffman, Arvore* raiz)
 {
-    if(Arvore_EhFolha(raiz))
+    if(Arvore_EhFolha(raiz)) // se for uma folha
     {
+        // Cria um struct caminho com o carácter e o caminho até ele
         Caminho* cam = (Caminho*) malloc(sizeof(Caminho));
         cam->caracter = Arvore_Caracter(raiz);
         cam->caminho = Arvore_Caminho(huffman,raiz);
+        // Adiciona o struct criado na lista
         Lista_ListaAdd(lista,Lista_NovoItem("Caminho",cam),0);
     }
-    else
+    else // se não for uma folha aplica a função recursivamente a suas árvores
     {
-        CriaCaminhos(lista,huffman,Arvore_ArvoreDireita(raiz));
-        CriaCaminhos(lista,huffman,Arvore_ArvoreEsquerda(raiz));
+        CriaCaminhos(lista,huffman,Arvore_ArvoreDireita(raiz)); // aplicando recursão a direita
+        CriaCaminhos(lista,huffman,Arvore_ArvoreEsquerda(raiz)); // aplicando recursão a esquerda
     }
 }
 
 // Cria a lista de caminhos da árvore de Huffman
 Lista* ListaCaminho_CriaLista(Arvore* huffman)
 {
-    Lista *novaLista = Lista_NovaLista("Caminho");
+    Lista *novaLista = Lista_NovaLista("Caminho"); // cria a lista
 
-    CriaCaminhos(novaLista,huffman,huffman);
+    CriaCaminhos(novaLista,huffman,huffman); // insere cada carácter da árvore na lista com seus respectivos caminhos
 
     return novaLista;
 }
@@ -70,26 +72,25 @@ Lista* ListaCaminho_CriaLista(Arvore* huffman)
 // Retorna o caminho de um carácter
 Lista* ListaCaminho_Caminho(Lista* listaCam, unsigned char caracter)
 {
-    int i;
-    Caminho* p;
-    for(i = 0 ; i < Lista_TamanhoLista(listaCam) ; i++)
+    int i; // variável de incrementação
+    Caminho* p; // variável auxiliar de busca
+    for(i = 0 ; i < Lista_TamanhoLista(listaCam) ; i++) // varre toda a lista
     {
-        p = Lista_AchaItem(listaCam,i);
-        if(p->caracter == caracter)
+        p = Lista_AchaItem(listaCam,i); // auxiliar recebe o struct caminho de um carácter
+        if(p->caracter == caracter) // se for o carácter buscado interrompe a busca
         {
             break;
         }
     }
-    return p->caminho;
+    return p->caminho; // retorna o caminho
 }
 
 // Destroi a lista de caminhos
 Lista* ListaCaminho_DestroiLista(Lista* lista)
 {
-    if(lista != NULL)
+    if(lista != NULL) // medida de segurança
     {
-        Lista_DestroiLista(lista,DestroiCaminho);
-        free(lista);
+        Lista_DestroiLista(lista,DestroiCaminho); // destroi a lista de structs
     }
     return NULL;
 }
